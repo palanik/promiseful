1ed927067ce8925799889146bb19bd393484c98a
palanik <palani.kumanan@gmail.com>
2017-04-13 09:53:39 -0400

    allow promise or value in place of function

--- src/index.js
+++ src/index.js
@@ -1,34 +1,51 @@
 class Promiseful {
+  static fulfil(val) {
+    if (typeof val === 'function') {
+      return val;
+    }
+
+    // Is this a Promise
+    if (typeof val === 'object'
+      && val.then
+      && typeof val.then === 'function') {
+      return () => val;
+    }
+
+    // Anything else, wrap into a promise into a function
+    return () => Promise.resolve(val);
+  }
+
   static parallel(funcs) {
     return Promiseful.all(funcs);
   }
 
   static all(funcs) {
-    return Promise.all(funcs.map(a => a()));
+    return Promise.all(funcs.map(a => Promiseful.fulfil(a)).map(a => a()));
   }
 
   static race(funcs) {
     if (funcs.length < 1) {
       return Promise.resolve(null);
     }
-    return Promise.race(funcs.map(a => a()));
+    return Promise.race(funcs.map(a => Promiseful.fulfil(a)).map(a => a()));
   }
 
   static series(funcs) {
     return new Promise((resolve, reject) => {
       function next(idx, acc) {
         if (idx >= funcs.length) {
-          return resolve(acc);
+          resolve(acc);
+          return;
         }
 
-        const p = funcs[idx]();
-        return p.then(
+        const p = Promiseful.fulfil(funcs[idx])();
+        p.then(
           (val) => {
             acc.push(val);
             next(idx + 1, acc);
-          },
-          reject,
-        );
+          }                     // eslint-disable-line comma-dangle
+        )
+        .catch(reject);
       }
 
       next(0, []);
