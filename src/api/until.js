b915ef48a07ef8a09b056084281930dc1b52f561
palanik <palani.kumanan@gmail.com>
2017-05-02 22:40:18 -0400

    pass value for break

--- src/api/until.js
+++ src/api/until.js
@@ -1,5 +1,5 @@
 import core from './core';
 
-export default function whilst(test, fn, initValue) {
-  return core.loop(fn, (v) => test(v), initValue);
+export default function until(test, fn, ...initValue) {
+  return core.loop(fn, (v) => test(v), ...initValue);
 }
