61244f426bf8e7f231586d172187c3aa4d95f0d9
palanik <palani.kumanan@gmail.com>
2017-05-26 11:04:08 -0400

    'latest' dist

--- rollup.config.js
+++ rollup.config.js
@@ -3,38 +3,38 @@ import babel from 'rollup-plugin-babel';
 import license from 'rollup-plugin-license';
 import uglify from 'rollup-plugin-uglify';
 
-const version = require('./package.json').version;
+const straight = (version = 'latest') => ({
+    entry: 'src/promiseful.js',
+    format: 'umd',
+    moduleName: 'promiseful',
+    plugins: [
+      babel({
+        exclude: 'node_modules/**'
+      }),
+      license({
+        banner: {
+          file: path.join(__dirname, 'LICENSE'),
+          encoding: 'utf-8',
+        }
+      })
+    ],
+    dest: `dist/${version}/promiseful.js`
+});
 
-const straight = {
-  entry: 'src/promiseful.js',
-  format: 'umd',
-  moduleName: 'promiseful',
-  plugins: [
-    babel({
-      exclude: 'node_modules/**'
-    }),
-    license({
-      banner: {
-        file: path.join(__dirname, 'LICENSE'),
-        encoding: 'utf-8',
-      }
-    })
-  ],
-  dest: `dist/${version}/promiseful.js`
-};
+const minify = (version = 'latest') => ({
+    entry: 'src/promiseful.js',
+    format: 'umd',
+    moduleName: 'promiseful',
+    plugins: [
+      babel({
+        exclude: 'node_modules/**'
+      }),
+      uglify()
+    ],
+    dest: `dist/${version}/promiseful.min.js`,
+    sourceMap: 'true'
+});
 
-const minify = {
-  entry: 'src/promiseful.js',
-  format: 'umd',
-  moduleName: 'promiseful',
-  plugins: [
-    babel({
-      exclude: 'node_modules/**'
-    }),
-    uglify()
-  ],
-  dest: `dist/${version}/promiseful.min.js`,
-  sourceMap: 'true'
-};
+const ver = (process.env.version) ? process.env.version : require('./package.json').version;
 
-export default (process.env.minify === 'true') ? minify : straight;
+export default (process.env.minify === 'true') ? minify(ver) : straight(ver);
